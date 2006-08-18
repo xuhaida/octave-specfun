cdbd7a7593e6d99509e7b6cb3aa108aab9cde8be
sis-sou <sis-sou@localhost>
2006-08-18 17:22:53 +0000

    Initial commit into CVS

---
+++ expint_Ei.m
@@ -0,0 +1,42 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## EXPINT_EI compute the exponential integral,
+##
+##                      infinity
+##                     /
+##       expint(x) = - | exp(t)/t dt
+##                     /
+##                     -x
+##
+## See also expint, expint_E1.
+
+function y = expint_Ei(x)
+	if (nargin != 1)
+		usage ("expint_Ei(x)");
+	endif
+	
+	if(x<0)
+		y = -quad("expintbase",-x,Inf);
+	else
+		y = 0;
+		for i = 1:100;
+			y = y + x.^i./(i.*factorial(i));
+		endfor
+
+		y = 0.577215664901532860606512090082402431 + log(x) + y;
+	endif;
+endfunction;
