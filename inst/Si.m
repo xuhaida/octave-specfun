6cb2c7ee13eabaaee9c7c1a0180c41507bf0fa52
sis-sou <sis-sou@localhost>
2006-09-07 18:13:23 +0000

    Initial commit into CVS.

---
+++ inst/Si.m
@@ -0,0 +1,40 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## SI compute the sine integral,
+##
+##                x
+##               /
+##    Si(x) =    | sin(t)/t dt
+##               /
+##               0
+##
+
+function y = Si(x)
+		if (nargin != 1)
+			usage ("Si(x)");
+	   endif
+		
+		if(imag(x) == 0)
+			F = @(x) sin(x)./x;
+			y = quad(F,0,x);
+		else
+			y = 0;
+			for i = 1:100;
+				y = y + (-1)^(i-1).*((x.^(2.*i-1))/((2.*i-1).*factorial(2.*i-1)));
+			endfor;
+		endif;
+endfunction;
