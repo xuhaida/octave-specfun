1bddeae59a5ce20fa0ff93666f565f2c73a2df15
sis-sou <sis-sou@localhost>
2006-09-29 17:46:10 +0000

    Initial commit into CVS.

---
+++ inst/heaviside.m
@@ -0,0 +1,35 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## HEAVISIDE compute the heaviside step function.
+## see also dirac.
+##
+
+function y = heaviside(x)
+		if (nargin != 1)
+			usage ("heaviside(x)");
+	   endif
+		
+		if(x == 0)
+			y = NaN;
+		else
+			if(x < 0)
+				y = 0;
+			else
+				y = 1;
+			endif
+		endif
+endfunction;
