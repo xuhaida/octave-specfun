f0c5e3fabe64215e719d6276028e0c2ea9a8d30b
sis-sou <sis-sou@localhost>
2006-09-29 17:40:20 +0000

    Initial commit into CVS.

---
+++ inst/dirac.m
@@ -0,0 +1,31 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## DIRAC compute the dirac delta function.
+## see also heaviside.
+##
+
+function y = dirac(x)
+		if (nargin != 1)
+			usage ("dirac(x)");
+	   endif
+		
+		if(x == 0)
+			y = Inf;
+		else
+			y = 0;
+		endif
+endfunction;
