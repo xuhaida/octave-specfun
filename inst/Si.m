8301df917a605447d1d31c4a3d3ef7f944c0da99
sis-sou <sis-sou@localhost>
2006-10-02 05:45:38 +0000

    Errors fixed.

--- inst/Si.m
+++ inst/Si.m
@@ -14,37 +14,17 @@
 ## along with this program; if not, write to the Free Software
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-## SI compute the sine integral,
-##
-##                x
-##               /
-##    Si(x) =    | sin(t)/t dt
-##               /
-##               0
-##
 
 function y = Si(x)
 		if (nargin != 1)
 			usage ("Si(x)");
 	   endif
-		s = size(x)(2);
-		for t = 1:s
-			if( x == 0)
-				y = 0;
-			else
-				if(imag(x(t)) == 0)
-					F = @(x) sin(x)./x;
-					y(t) = quad(F,0,x(t));
-				else
-					y(t) = 0;
-					for k = 0:100
-						y(t) = y(t) + (besselj(k + 0.5,x(t)./2)).^2;
-					endfor
-					y(t) = y(t).*pi;
-					if(real(x(t))==0)
-						y(t)=i.*imag(y(t));
-					endif
-				endif
-			endif
+		y = 0;
+
+		for k = 0:100
+			y = y + (besselj(k + 0.5,x./2)).^2;
 		endfor
-endfunction
+		y = y.*pi;
+		y(x == 0) = 0;
+		y(real(x)==0) = i.*imag(y(real(x)==0));
+endfunction;
