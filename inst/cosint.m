97f676110431f952be0827b15efeb9922ecc603b
sis-sou <sis-sou@localhost>
2006-10-17 11:32:30 +0000

    Initial commit into CVS.

---
+++ inst/cosint.m
@@ -0,0 +1,32 @@
+## Copyright (C) 2006   Sissou   <sylvain.pelissier@gmail.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+## COSINT compute the cosine integral function define by:
+##
+##                    Inf
+##                   /
+##       cosint(x) = | cos(t)/t dt
+##                   /
+##                   x
+##
+##See also : Ci, Si, sinint, expint, expint_Ei.
+
+function y = cosint(z)
+	if (nargin != 1)
+		usage ("cosint(x)");
+	endif
+	y = Ci(z);
+endfunction;
